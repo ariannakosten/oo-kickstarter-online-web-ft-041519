da4e954308bc0eef21a3e3c393fd49818f595a3a
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-11 06:40:01 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,8 +9,8 @@ class Project
   
   def add_backer(backer)
     @backers << backer                #backer arg added to the Project backers array
-    #backer.backed_projects << self  #backer arg .Backer- backed projects array adds self
-    backed_projects << self.backer
+    backer.backed_projects << self  #backer arg .Backer- backed projects array adds self
+    #@backed_projects << backer.self
   end
   
 end
